3ee19e52f55c85bf01a14cc4ae75c1306d6fc8cd
Ash Smith <ash.smith@meanbee.com>
2015-08-16 18:05:46 +0100

    Create our Data Interface Service Contract

---
+++ Api/Data/PostInterface.php
@@ -0,0 +1,122 @@
+<?php
+namespace Ashsmith\Blog\Api\Data;
+
+
+interface PostInterface
+{
+    /**
+     * Constants for keys of data array. Identical to the name of the getter in snake case
+     */
+    const POST_ID       = 'post_id';
+    const URL_KEY       = 'url_key';
+    const TITLE         = 'title';
+    const CONTENT       = 'content';
+    const CREATION_TIME = 'creation_time';
+    const UPDATE_TIME   = 'update_time';
+    const IS_ACTIVE     = 'is_active';
+
+    /**
+     * Get ID
+     *
+     * @return int|null
+     */
+    public function getId();
+
+    /**
+     * Get URL Key
+     *
+     * @return string
+     */
+    public function getUrlKey();
+
+    /**
+     * Get title
+     *
+     * @return string|null
+     */
+    public function getTitle();
+
+    /**
+     * Get content
+     *
+     * @return string|null
+     */
+    public function getContent();
+
+    /**
+     * Get creation time
+     *
+     * @return string|null
+     */
+    public function getCreationTime();
+
+    /**
+     * Get update time
+     *
+     * @return string|null
+     */
+    public function getUpdateTime();
+
+    /**
+     * Is active
+     *
+     * @return bool|null
+     */
+    public function isActive();
+
+    /**
+     * Set ID
+     *
+     * @param int $id
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setId($id);
+
+    /**
+     * Set URL Key
+     *
+     * @param string $url_key
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setUrlKey($url_key);
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setTitle($title);
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setContent($content);
+
+    /**
+     * Set creation time
+     *
+     * @param string $creationTime
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setCreationTime($creationTime);
+
+    /**
+     * Set update time
+     *
+     * @param string $updateTime
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setUpdateTime($updateTime);
+
+    /**
+     * Set is active
+     *
+     * @param int|bool $is_active
+     * @return \Ashsmith\Blog\Api\Data\PostInterface
+     */
+    public function setIsActive($is_active);
+}
