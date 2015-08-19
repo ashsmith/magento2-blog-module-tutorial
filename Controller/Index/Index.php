1f04e7f639ed8adef9ba62938ae09a79a9173723
Ash Smith <ash.smith@meanbee.com>
2015-08-19 08:25:26 +0100

    Create controller

---
+++ Controller/Index/Index.php
@@ -0,0 +1,29 @@
+<?php
+namespace Ashsmith\Blog\Controller\Index;
+
+use \Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    /** @var  \Magento\Framework\View\Result\Page */
+    protected $resultPageFactory;
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(\Magento\Framework\App\Action\Context $context,
+                                \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Blog Index, shows a list of recent blog posts.
+     *
+     * @return \Magento\Framework\View\Result\PageFactory
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
