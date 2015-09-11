869a4376acba8c525014918f97660c90da4b8dd3
Ash Smith <ash.smith@meanbee.com>
2015-09-11 09:25:45 +0100

    Remove AbstractMassDelete.php
    No point in having an abstract class for this. Only useful if you're going
    to have multiple Delete actions for your module. Ours doesn't

--- Controller/Adminhtml/Post/MassDelete.php
+++ Controller/Adminhtml/Post/MassDelete.php
@@ -1,17 +1,23 @@
 <?php
 namespace Ashsmith\Blog\Controller\Adminhtml\Post;
 
-use Ashsmith\Blog\Controller\Adminhtml\AbstractMassDelete;
+use Magento\Framework\Model\Resource\Db\Collection\AbstractCollection;
+use Magento\Framework\Controller\ResultFactory;
 
 /**
  * Class MassDelete
  */
-class MassDelete extends AbstractMassDelete
+class MassDelete extends \Magento\Backend\App\Action
 {
     /**
      * Field id
      */
     const ID_FIELD = 'post_id';
+
+    /**
+     * Redirect url
+     */
+    const REDIRECT_URL = '*/*/';
 
     /**
      * Resource collection
@@ -26,4 +32,109 @@ class MassDelete extends AbstractMassDelete
      * @var string
      */
     protected $model = 'Ashsmith\Blog\Model\Post';
+    /**
+     * Execute action
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $selected = $this->getRequest()->getParam('selected');
+        $excluded = $this->getRequest()->getParam('excluded');
+
+        try {
+            if (isset($excluded)) {
+                if (!empty($excluded)) {
+                    $this->excludedDelete($excluded);
+                } else {
+                    $this->deleteAll();
+                }
+            } elseif (!empty($selected)) {
+                $this->selectedDelete($selected);
+            } else {
+                $this->messageManager->addError(__('Please select item(s).'));
+            }
+        } catch (\Exception $e) {
+            $this->messageManager->addError($e->getMessage());
+        }
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath(static::REDIRECT_URL);
+    }
+
+    /**
+     * Delete all
+     *
+     * @return void
+     * @throws \Exception
+     */
+    protected function deleteAll()
+    {
+        /** @var AbstractCollection $collection */
+        $collection = $this->_objectManager->get($this->collection);
+        $this->setSuccessMessage($this->delete($collection));
+    }
+
+    /**
+     * Delete all but the not selected
+     *
+     * @param array $excluded
+     * @return void
+     * @throws \Exception
+     */
+    protected function excludedDelete(array $excluded)
+    {
+        /** @var AbstractCollection $collection */
+        $collection = $this->_objectManager->get($this->collection);
+        $collection->addFieldToFilter(static::ID_FIELD, ['nin' => $excluded]);
+        $this->setSuccessMessage($this->delete($collection));
+    }
+
+    /**
+     * Delete selected items
+     *
+     * @param array $selected
+     * @return void
+     * @throws \Exception
+     */
+    protected function selectedDelete(array $selected)
+    {
+        /** @var AbstractCollection $collection */
+        $collection = $this->_objectManager->get($this->collection);
+        $collection->addFieldToFilter(static::ID_FIELD, ['in' => $selected]);
+        $this->setSuccessMessage($this->delete($collection));
+    }
+
+    /**
+     * Delete collection items
+     *
+     * @param AbstractCollection $collection
+     * @return int
+     */
+    protected function delete(AbstractCollection $collection)
+    {
+        $count = 0;
+        foreach ($collection->getAllIds() as $id) {
+            /** @var \Magento\Framework\Model\AbstractModel $model */
+            $model = $this->_objectManager->get($this->model);
+            $model->load($id);
+            $model->delete();
+            ++$count;
+        }
+
+        return $count;
+    }
+
+    /**
+     * Set error messages
+     *
+     * @param int $count
+     * @return void
+     */
+    protected function setSuccessMessage($count)
+    {
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $count));
+    }
 }
