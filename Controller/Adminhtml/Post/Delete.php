d799f82991e7f93fe4a9b03dd40b63b674cc099b
Ash Smith <ash.smith@meanbee.com>
2016-01-15 10:53:50 +0000

    Fix #11: Ability to delete posts from edit screen
    When not using a mass action you were unable to delete blog posts.

---
+++ Controller/Adminhtml/Post/Delete.php
@@ -0,0 +1,43 @@
+<?php
+namespace Ashsmith\Blog\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action;
+use Magento\TestFramework\ErrorLog\Logger;
+
+class Delete extends \Magento\Backend\App\Action
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Ashsmith_Blog::delete');
+    }
+
+    /**
+     * Delete action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $id = $this->getRequest()->getParam('post_id');
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($id) {
+            try {
+                $model = $this->_objectManager->create('Ashsmith\Blog\Model\Post');
+                $model->load($id);
+                $model->delete();
+                $this->messageManager->addSuccess(__('The post has been deleted.'));
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Exception $e) {
+                $this->messageManager->addError($e->getMessage());
+                return $resultRedirect->setPath('*/*/edit', ['post_id' => $id]);
+            }
+        }
+        $this->messageManager->addError(__('We can\'t find a post to delete.'));
+        return $resultRedirect->setPath('*/*/');
+    }
+}
